a50ca6730d87ac36107141380a7d31dd3d4372a3
AkiBoba <lettervl@ya.ru>
2022-02-18 17:55:31 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/Parse.java
+++ src/main/java/ru/job4j/grabber/Parse.java
@@ -1,12 +1,10 @@
 package ru.job4j.grabber;
 
 import ru.job4j.models.Post;
-
-import java.io.IOException;
 import java.util.List;
 
 public interface Parse {
-    List<Post> list(String link) throws IOException;
+    List<Post> list(String link);
 
-    Post detail(String link) throws IOException;
+    Post detail(String link);
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -24,33 +24,44 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
+    public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
         for (int page = 1; page < 6; page++) {
-            link.concat(String.format("%s", page));
-            Document doc = Jsoup.connect(link).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                if (href.attr("href").toLowerCase(Locale.ROOT).contains("java")) {
-                    posts.add(detail(href.attr("href")));
+            try {
+                Document doc = Jsoup.connect(String.format(link + "%s", page)).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Element href = td.child(0);
+                    if (href.attr("href").toLowerCase(Locale.ROOT).contains("java")
+                            && !href.attr("href").toLowerCase(Locale.ROOT).contains("javascript")) {
+                        posts.add(detail(href.attr("href")));
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
         return posts;
     }
 
-    public Post detail(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Element first = doc.selectFirst(".msgTable");
-        String title = first.select(".messageHeader").get(0).ownText();
-        String description = first.select(".msgBody").get(1).ownText();
-        String date = first.select(".msgFooter").get(0).ownText();
-        LocalDateTime created = dateTimeParser.parse(date.substring(0, date.indexOf("[")));
+    public Post detail(String url) {
+        String title = null;
+        String description = null;
+        LocalDateTime created = null;
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element first = doc.selectFirst(".msgTable");
+            title = first.select(".messageHeader").get(0).ownText();
+            description = first.select(".msgBody").get(1).ownText();
+            String date = first.select(".msgFooter").get(0).ownText();
+            created = dateTimeParser.parse(date.substring(0, date.indexOf("[")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Post(title, url, description, created);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String url = "https://www.sql.ru/forum/job-offers/";
         DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
         Parse parse = new SqlRuParse(dateTimeParser);
