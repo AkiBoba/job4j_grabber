debd79d8962edf9709e9c20b527773e33e399179
Vladimir Likhachev <lettervl@ya.ru>
2022-02-21 21:55:09 +0300

    1. Принципы Kiss, Dry и Yagni

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,6 +1,5 @@
 package ru.job4j.kiss;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 /**
--- src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.kiss;
 
 import junit.framework.TestCase;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
