2ba57174932fcdd37bb930662c2f3ed67dad7b3f
Vladimir Likhachev <lettervl@ya.ru>
2022-02-22 09:58:40 +0300

    1. Принципы Kiss, Dry и Yagni

--- src/main/java/ru/job4j/kiss/MaxMin.java
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -8,12 +8,15 @@ import java.util.List;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        value.sort(comparator);
-        return value.get(value.size() - 1);
+        return minmax(value, comparator, value.size() - 1);
     }
 
     public <T> T min(List<T> value, Comparator<T> comparator) {
+        return minmax(value, comparator, 0);
+    }
+
+    static <T> T minmax(List<T> value, Comparator<T> comparator, int index) {
         value.sort(comparator);
-        return value.get(0);
+        return value.get(index);
     }
 }
